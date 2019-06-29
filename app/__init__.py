a1735c3b7cb05b11beea32b7782dcb467ddbac0e
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-29 22:59:00 +0300

    added manager methods in the init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,6 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginMaager
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
