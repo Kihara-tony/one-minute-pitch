b3c70f52f6085db13f22a668b0c3530abe6715cc
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-29 21:45:14 +0300

    added a class for the form in the forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,7 @@ from ..models import User
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us something about you that you may like to share.', validators=[Required()])
     submit = SubmitField('Submit')
+class GeneralForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
