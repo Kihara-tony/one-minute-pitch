4134f7a9964e1b4ca7af579f447d8f1270494f96
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-30 10:47:39 +0300

    added a function for veryfying the  password.

--- manage.py
+++ manage.py
@@ -19,3 +19,8 @@ class User(UserMixin,db.Model):
     @property
     def password(self):
         raise AttributeError('You Cannot read the password attribute')
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
